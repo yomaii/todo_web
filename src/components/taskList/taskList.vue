9e5d9cb3d250a4ce76b3fd83ff0d26fb687a895a
lolly <i@lolly.cc>
2022-10-03 23:39:07 +0900

    feat:add finish todo and only show unfinished todos

--- src/components/taskList/taskList.vue
+++ src/components/taskList/taskList.vue
@@ -1,20 +1,27 @@
 <script setup>
 import { computed } from "vue";
+import { useTodosStore } from "../../stores/todos";
 import Item from "./taskListItem.vue";
-const props = defineProps(["data", "showFinished"]);
-const filteredTodos = computed(() => {
-  return props.showFinished
-    ? props.data.filter((t) => !t.finished)
-    : props.data;
-});
+const todos = useTodosStore();
+let filteredTodos = computed(() => todos.filterTodos());
+function setFilterForUnfinished() {
+  todos.filter = (todos) => todos.filter((todo) => !todo.finished);
+}
 </script>
 
 <template>
   <ul class="tasklist">
+    <el-row class="mb-4">
+      <el-button v-if="todos.filter" @click="todos.clearFilter"
+        >显示全部</el-button
+      >
+      <el-button v-else @click="setFilterForUnfinished">显示未完成</el-button>
+    </el-row>
     <Item
       v-for="todo in filteredTodos"
       :key="todo.id"
       :finished="todo.finished"
+      @click="todos.changeTodoStatus(todo)"
       >{{ todo.title }}</Item
     >
   </ul>
